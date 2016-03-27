ffff5c423bca6468971bdab52c6523b9d8531f13
Крис <chris@codecraft.ch>
2016-03-27 15:59:47 +0200

    V Builders

--- src/v_builders/_39_HtmlBuilders.kt
+++ src/v_builders/_39_HtmlBuilders.kt
@@ -6,35 +6,47 @@ import v_builders.data.getProducts
 import v_builders.htmlLibrary.*
 
 fun getTitleColor() = "#b9c9fe"
-fun getCellColor(index: Int, row: Int) = if ((index + row) %2 == 0) "#dce4ff" else "#eff2ff"
+fun getCellColor(index: Int, row: Int) = if ((index + row) % 2 == 0) "#dce4ff" else "#eff2ff"
 
 fun todoTask39(): Nothing = TODO(
-    """
+        """
         Task 39.
         1) Fill the table with the proper values from products.
         2) Color the table like a chess board (using getTitleColor() and getCellColor() functions above).
         Pass a color as an argument to functions 'tr', 'td'.
         You can run the 'Html Demo' configuration in IntelliJ to see the rendered table.
     """,
-    documentation = doc39()
+        documentation = doc39()
 )
 
 fun renderProductTable(): String {
     return html {
         table {
             tr {
-                td {
+                td(color = getTitleColor()) {
                     text("Product")
                 }
-                td {
+                td(color = getTitleColor()) {
                     text("Price")
                 }
-                td {
+                td(color = getTitleColor()) {
                     text("Popularity")
                 }
             }
             val products = getProducts()
-            todoTask39()
+            for ((index, product) in products.withIndex()) {
+                tr {
+                    td(color = getCellColor(index, 0)) {
+                        text(product.description)
+                    }
+                    td(color = getCellColor(index, 1)) {
+                        text(product.price)
+                    }
+                    td(color = getCellColor(index, 2)) {
+                        text(product.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
